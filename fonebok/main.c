f8b11e58accc68549bf9019157d7272106e640a6
Junell <dowd.junell@gmail.com>
2019-07-09 11:42:23 -0400

    Created a main file in the project fonebok

---
+++ fonebok/main.c
@@ -0,0 +1,23 @@
+/*
+Junell
+----------------------------------------------------------------------
+ */
+
+/* 
+ * File:   main.c
+ * Author: Junell
+ *
+ * Created on July 9, 2019, 11:30 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
