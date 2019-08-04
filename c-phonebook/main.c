20bac4c4713c3fbea991f595d59286105a1c15b4
Mark Tov <mark@tovster.me>
2019-08-04 16:56:26 -0400

    Basic menu, with validation

--- c-phonebook/main.c
+++ c-phonebook/main.c
@@ -9,45 +9,6 @@
 
 int main() {
     record_t record;
-    char temp;
-    printf("Please choose an option: ");
-    scanf("%c", temp);
-    if (temp != '\n') {
-        switch (temp) {
-            case '1':
-                printf("Add");
-                FLUSH;
-                //do something
-                break;
-            case '2':
-                printf("View");
-                FLUSH;
-                //do something
-                break;
-            case '3':
-                printf("Modify");
-                FLUSH;
-                //do something
-                break;
-            case '4':
-                printf("Delete");
-                FLUSH;
-                //do something
-                break;
-            case '5':
-                printf("Search");
-                FLUSH;
-                //do something
-                break;
-            case '6':
-                printf("Goodbye!");
-                exit(0);
-            default:
-                printf("Invalid input, please try again\n\n");
-        }
-    } else {
-        printf("Invalid input, please try again\n\n");
-    }
-    return 0;
+    menu(&record);
 }
 
--- c-phonebook/menu.h
+++ c-phonebook/menu.h
@@ -6,9 +6,33 @@
 #define C_PHONEBOOK_MENU_H
 
 #include <stdio.h>
+#define FLUSH stdin=freopen(NULL, "r", stdin)
 
-void menu(record_t *);
+void menu(record_t*);
 
+void menu(record_t *root) {
+    printf("********** 📞 CONTACTS **********\n\n");
+    printf("Please select a menu option\n");
+    printf("1.Add\t\t2.View All\t\t3.Modify\n");
+    printf("4.Delete\t5.Search\t\t6.Exit\n\n");
 
+    int menuOption = 0;
+    while (1) {
+        char *userInput;
+        printf("Please select an option: ");
+        fgets(userInput, sizeof(int), stdin);
+        menuOption = strtol(userInput, NULL, 0);
+        if (menuOption >= 1) {
+            if(menuOption <= 6) {
+                printf("\n%d, is your option", menuOption);
+                break;
+            } else {
+                printf("Invalid option, please try again\n");
+            }
+        } else {
+            printf("Invalid option, please try again.\n");
+        }
+    }
+}
 
 #endif //C_PHONEBOOK_MENU_H
