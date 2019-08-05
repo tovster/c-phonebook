29639bc2fa1f3f08a9f8a38bb7712d4d68635421
Mark Tov <mark@tovster.me>
2019-08-05 13:33:42 -0400

    Names in records work now

--- c-phonebook/recordlib.h
+++ c-phonebook/recordlib.h
@@ -4,6 +4,7 @@
 
 #ifndef C_PHONEBOOK_RECORDLIB_H
 #define C_PHONEBOOK_RECORDLIB_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,12 +14,19 @@
 #define MAX_LEN 25
 
 record_t *createRecord();
+
 record_t *createList();
+
 record_t *rebuildList(record_t *root);
+
 void displayList(record_t *root);
+
 void deleteRecord(record_t *record);
+
 void deleteList(record_t *root);
+
 record_t modifyRecord(record_t *record);
+
 record_t searchRecord(int id, char *name);
 
 record_t *createRecord() {
@@ -33,10 +41,10 @@ record_t *createRecord() {
     // filling id
     int id;
 
-    while(1) {
+    while (1) {
         char tempId;
-        printf("Enter ID number: ");
         FLUSH;
+        printf("Enter ID number: ");
         fgets(&tempId, MAX_LEN, stdin);
 
         id = strtol(&tempId, NULL, 0);
@@ -55,7 +63,32 @@ record_t *createRecord() {
     memcpy(&record->id, &id, sizeof(&id));  // copying the id into the record
 
     //filling name
-    printf("Id Num = %d", record->id);
+
+    while (1) {
+        char tempName[MAX_LEN];
+        FLUSH;
+        printf("Please enter a name: ");
+        fgets(tempName, MAX_LEN, stdin);
+
+        if (strlen(tempName) > 1) {
+            record->name = (char *) malloc(sizeof(tempName) + 1);
+            if (record->name == NULL) {
+                printf("Can't allocate memory for this record's name!");
+                free(record->id);
+                free(record);
+                record = NULL;
+                return record;
+            }
+            strncpy(record->name, tempName, sizeof(tempName));
+            record->name[strcspn(record->name, "\n")] = 0;
+            break;
+        } else {
+            printf("Invalid input! Please try again.");
+        }
+    }
+
+    printf("ID: %d,\n", record->id);
+    printf("Name: %s\n", record->name);
 
     return record;
 
