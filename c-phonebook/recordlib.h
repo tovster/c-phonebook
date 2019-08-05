90f3c1abe032eb7a01f6f890fec0e2919ae64f93
Mark Tov <mark@tovster.me>
2019-08-05 10:54:37 -0400

    creating records, with id number

--- c-phonebook/recordlib.h
+++ c-phonebook/recordlib.h
@@ -4,7 +4,11 @@
 
 #ifndef C_PHONEBOOK_RECORDLIB_H
 #define C_PHONEBOOK_RECORDLIB_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "record.h"
+#define MAX_LEN 25
 
 record_t *createRecord();
 record_t *createList();
@@ -14,5 +18,34 @@ void deleteRecord(record_t *record);
 void deleteList(record_t *root);
 record_t modifyRecord(record_t *record);
 record_t searchRecord(int id, char *name);
+
+record_t *createRecord() {
+
+    // mallocing record_t
+    record_t *record = (record_t *) malloc(sizeof(record_t));
+    if (record == NULL) {
+        printf("Can not allocate memory for this contact.\n");
+        return record;
+    }
+
+    // filling id
+    int id;
+    while(1) {
+        char tempId;
+        printf("Enter ID number: ");
+        fgets(&tempId, MAX_LEN, stdin);
+
+        id = strtol(&tempId, NULL, 0);
+        if (id > 0) {
+            break;
+        } else {
+            printf("Incorrect value! Please try again.");
+        }
+    }
+
+    memcpy(&record->id, &id, sizeof(&id));  // copying the id into the record
+
+    return &record;
+}
 
 #endif //C_PHONEBOOK_RECORDLIB_H
