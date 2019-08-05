6b3d7ef088c511f10e799c795b004d277c49d26e
Mark Tov <mark@tovster.me>
2019-08-05 16:12:48 -0400

    Started work on pbutils, prepped functions

--- c-phonebook/pbutils.h
+++ c-phonebook/pbutils.h
@@ -7,4 +7,20 @@
 
 #define FLUSH stdin=freopen(NULL, "r", stdin)
 
+int txtSort (char*, char*);
+int numSort (int*, int*);
+record_t* sortRecordList (record_t*, char);
+
+int txtSort (char *string1, char *string2) {
+    return 1;
+}
+
+int numSort (int *number1, int *number2) {
+    return 1;
+}
+
+record_t* sortRecordList (record_t* head, char sortBy) {
+    return NULL;
+}
+
 #endif //C_PHONEBOOK_PBUTILS_H
