fc7a90131486144619f07b92a176788168702178
Mark Tov <mark@tovster.me>
2019-08-06 15:02:37 -0400

    created the record_bst structure

--- c-phonebook/recordbst.h
+++ c-phonebook/recordbst.h
@@ -4,5 +4,12 @@
 
 #ifndef C_PHONEBOOK_RECORDBST_H
 #define C_PHONEBOOK_RECORDBST_H
+#include "record.h"
+
+struct record_bst {
+    record_t *left;
+    record_t *node;
+    record_t *right;
+} recordbst_t;
 
 #endif //C_PHONEBOOK_RECORDBST_H
